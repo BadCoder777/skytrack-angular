3fb5d170bb3779f91e367d1fa2f724cb76e57ce6
Bad-Coder <badcoder.of@gmail.com>
2025-07-12 09:25:05 +0200

    fixed

--- src/app/data/services/flight.service.ts
+++ src/app/data/services/flight.service.ts
@@ -1,22 +1,21 @@
-import {inject, Injectable} from '@angular/core'
-import {HttpClient} from '@angular/common/http'
-import {IFlight} from '../../models/flight.model'
-import {BASIC_API_URL} from '../constants/BASIC_API_URL'
-import {ActivatedRoute} from '@angular/router'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IFlight } from '../../models/flight.model';
+import { BASIC_API_URL } from '../constants/BASIC_API_URL';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FlightService {
-  
-  public data: IFlight[] | IFlight = []
+  public data: IFlight[] | IFlight = [];
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {}
-  
+
   getFlights() {
-    return this.http.get<IFlight[]>(`${BASIC_API_URL}/flights`)
+    return this.http.get<IFlight[]>(`${BASIC_API_URL}/flights`);
   }
   getFlightById(id: number) {
-    return this.http.get<IFlight[]>(`${BASIC_API_URL}/flights?id=${id}`)
+    return this.http.get<IFlight[]>(`${BASIC_API_URL}/flights?id=${id}`);
   }
 }
